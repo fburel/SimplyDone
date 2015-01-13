a63d6c40c324e2124b65aec01b1eb2cb8553e4e8
Florian BUREL <florian.burel@mac.com>
2015-01-13 13:02:04 +0100

    Initial Commit

---
+++ SimplyDone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimplyDone
+//
+//  Created by Florian BUREL on 13/01/2015.
+//  Copyright (c) 2015 Florian Burel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimplyDone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimplyDone
+//
+//  Created by Florian BUREL on 13/01/2015.
+//  Copyright (c) 2015 Florian Burel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
